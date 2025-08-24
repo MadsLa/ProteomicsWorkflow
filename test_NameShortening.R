2d99fb0a744e53dff7880a16d9bb803955cf0daf
MadsLa <mads@bjerknaes.dk>
2025-08-24 20:51:10 +0200

    20250824-2

--- test_NameShortening.R
+++ test_NameShortening.R
@@ -3,22 +3,30 @@ source("functions.R")
 file = "raw/20220909_144243_20210817_XP1_YABH_MouseHF_PlasmaProt_Top2MDe_ProtPepMDBL_Report.tsv"
 dfBGS <- data.table::fread(file)
 
-dfBGS %>% 
+
+MDBL_NameShortening <- function(data){
+  data %>% 
   distinct(R.FileName) %>% 
   mutate(NameLength=str_length(R.FileName)) -> t
 
 for(n in max(t$NameLength):3){
-  dfBGS %>% 
+  data %>% 
     mutate(name = str_trunc(R.FileName, n, ellipsis = "")) %>%  
     distinct(name) %>% nrow() -> s
   if(s==1){
     print(n)
     dfBGS %>% 
       mutate(name = str_trunc(R.FileName, n, ellipsis = "")) %>%  
-      distinct(name) %>% pull(name)
+      distinct(name) %>% pull(name) -> k
+    print(k)
     break 
   }
   
 }
+  data %>% 
+    mutate(R.FileName = str_remove(R.FileName, k)) -> data
+  return(data)
+}
 
-  
+dfBGS %>% 
+  MDBL_ProteinsPerFileBoxplot()
